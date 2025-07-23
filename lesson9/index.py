2307f15b95551df358901477581bd03731a86c57
李東霖 <210831059+tereshiaa@users.noreply.github.com>
2025-07-23 13:56:13 +0000

    0723

---
+++ lesson9/index.py
@@ -0,0 +1,26 @@
+import box
+
+
+def main():
+    try:
+        play_count = 0
+        while(True):
+            play_count += 1
+            box.play_game()
+            is_continue = input("您還要繼續嗎(y,n)?")
+            if is_continue == "n":
+                break
+            
+        print(f"{box.user_name}共玩了{play_count}次")
+        print("遊戲結束")
+    except ValueError:
+        print("格式錯誤")
+        print("應用程式中斷")        
+    except Exception as e:
+        print(e)
+        print("應用程式中斷")
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ lesson9/功課5.py
@@ -0,0 +1,88 @@
+'''
+def get_status(prompt, min, max):
+    
+    value = float(input(prompt))
+    if not (min <= value <= max):
+        print(f'輸入錯誤，應在範圍 {min} 到 {max}')
+            
+        
+ 
+       
+
+def calculate_bmi(height, weight):
+    return weight / ((height / 100) ** 2)
+
+def get_bmi(bmi):
+    if bmi < 18.5:
+        return '體重過輕'
+    elif bmi < 24:
+        return '正常範圍'
+    elif bmi < 27:
+        return '體重過重'
+    elif bmi < 30:
+        return '輕度肥胖'
+    elif bmi < 35:
+        return '中度肥胖'
+    else:
+        return '重度肥胖'
+
+def main():
+    try:
+        height = get_status('請輸入身高（120-250cm）:', 120, 250)
+        weight = get_status('請輸入體重（30-200kg）:', 30, 200)
+
+        if height is not None and weight is not None:
+            bmi = calculate_bmi(height, weight)
+            print('你的BMI為:', round(bmi, 1))
+            print('您的BMI為', get_bmi(bmi))
+    except ValueError:
+        print('格式錯誤')
+    except Exception as e:
+        print(f'e')
+    
+    print('應用程式結束')
+
+# 執行主程式
+main()
+'''
+
+
+
+def get_bmi(BMI):
+    if BMI < 18.5:
+        return '體重過輕'
+    elif 18.5 <= BMI < 24:
+        return '正常範圍'
+    elif 24 <= BMI < 27:
+        return '體重過重'
+    elif 27 <= BMI < 30:
+        return '輕度肥胖'
+    elif 30 <= BMI < 35:
+        return '中度肥胖'
+    else:
+        return '重度肥胖'
+
+def main():
+    try:
+        height = float(input('請輸入身高 (120-250 cm): '))
+        if not (120 <= height <= 250):
+            print('身高範圍輸入錯誤')
+            return
+        
+        weight = float(input('請輸入體重 (30-200 kg): '))
+        if not (30 <= weight <= 200):
+            print('體重範圍輸入錯誤')
+            return
+
+        BMI = weight / ((height / 100) ** 2)
+        print('你的BMI為:', round(BMI, 1))
+
+        category = get_bmi(BMI)
+        print('您的BMI分類為:', category)
+
+    except:
+        print('輸入格式錯誤')
+    finally:
+        print('應用程式結束')
+
+main()
