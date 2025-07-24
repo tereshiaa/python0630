1af41c4bbcf6beb6b00d8b55aa1165013c5d41e3
李東霖 <210831059+tereshiaa@users.noreply.github.com>
2025-07-24 03:07:28 +0000

    0724

--- lesson9/功課5.py
+++ lesson9/功課5.py
@@ -48,41 +48,50 @@ main()
 
 
 
-def get_bmi(BMI):
-    if BMI < 18.5:
-        return '體重過輕'
-    elif 18.5 <= BMI < 24:
-        return '正常範圍'
-    elif 24 <= BMI < 27:
-        return '體重過重'
-    elif 27 <= BMI < 30:
-        return '輕度肥胖'
-    elif 30 <= BMI < 35:
-        return '中度肥胖'
+def get_bmi(height, weight):
+    # 計算 BMI 並返回
+    if 120 <= height <= 250 and 30 <= weight <= 200:
+        BMI = weight / ((height / 100) ** 2)
+        print('你的 BMI 為:', round(BMI, 1))
+        return BMI
     else:
-        return '重度肥胖'
+        print('身高或體重範圍錯誤')
+        return 
+
+def get_status(BMI):
+    if BMI is not None:
+        if BMI < 18.5:
+            x = '體重過輕'
+        elif 18.5 <= BMI < 24:
+            x = '正常範圍'
+        elif 24 <= BMI < 27:
+            x = '體重過重'
+        elif 27 <= BMI < 30:
+            x = '輕度肥胖'
+        elif 30 <= BMI < 35:
+            x = '中度肥胖'
+        else:
+            x = '重度肥胖'
+        print('您的 BMI 狀態為:', x)
+    
 
 def main():
     try:
-        height = float(input('請輸入身高 (120-250 cm): '))
+        height = float(input('請輸入身高 120-250 cm:'))
         if not (120 <= height <= 250):
             print('身高範圍輸入錯誤')
             return
-        
-        weight = float(input('請輸入體重 (30-200 kg): '))
+        weight = float(input('請輸入體重 30-200 kg:'))
         if not (30 <= weight <= 200):
             print('體重範圍輸入錯誤')
-            return
+            return       
+        BMI = get_bmi(height, weight)  # 呼叫 get_bmi 計算 BMI
+        get_status(BMI)  # 呼叫 get_status 顯示 BMI 狀態
 
-        BMI = weight / ((height / 100) ** 2)
-        print('你的BMI為:', round(BMI, 1))
-
-        category = get_bmi(BMI)
-        print('您的BMI分類為:', category)
-
-    except:
-        print('輸入格式錯誤')
+    except ValueError:
+        print('輸入格式錯誤，請輸入數字')
     finally:
         print('應用程式結束')
 
-main()
+if __name__ == "__main__":   #主執行檔
+    main()
